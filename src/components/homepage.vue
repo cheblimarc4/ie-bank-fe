5e7053555e072c5f73da3b944e15b4a81803be42
cheblimarc4 <cheblimarc4@gmail.com>
2023-10-06 18:00:42 +0200

    hotfix3

--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -1,32 +1,31 @@
-
 <template>
     <div class="home-page">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <div><h1>Welcome To Your Home Page</h1></div>
-      <div class="centered-box">
-        <h2>Welcome to the Bank App</h2>
-        <p>Would you like to go to the Accounts page to manage your finances?</p>
-        <router-link to="/accounts">Go to Accounts</router-link>
-      </div>
+      <h1>Welcome To Your Home Page</h1>
+      <div class="content">
+        <div class="centered-box">
+          <h2>Welcome to the Bank App</h2>
+          <p>Would you like to go to the Accounts page to manage your finances?</p>
+          <router-link to="/accounts">Go to Accounts</router-link>
+        </div>
   
-      <div class="centered-box">
-        <h2>Explore the Skull Component</h2>
-        <p>Check out our interactive Skull component for fun!</p>
-        <router-link to="/skull">Explore</router-link>
+        <div class="centered-box">
+          <h2>Explore the Skull Component</h2>
+          <p>Check out our interactive Skull component for fun!</p>
+          <router-link to="/skull">Explore</router-link>
+        </div>
       </div>
-      <div class = "logo">
+      <div class="logo">
         <img src="../assets/IE-University-logo.jpg" alt="Logo">
       </div>
-  
       <!-- <div class="centered-box creative-box">
         <h2>Be Creative!</h2>
         <p>Add your own creative content here.</p>
         <router-link to="/creative-content">Get Inspired</router-link>
       </div> -->
-    <router-view></router-view>
-
+      <router-view></router-view>
     </div>
   </template>
+  
   
   <script>
   export default {
@@ -35,48 +34,70 @@
   </script>
   
   <style scoped>
-  /* Remove default margin and padding from the body and html elements */
-body, html {
+/* Reset margins and padding for all elements */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* Apply the background color to the whole page */
+body {
+  background-color: lightblue;
   margin: 0;
   padding: 0;
 }
 
-/* Apply the background color and set min-height to 100vh on your .home-page element */
+/* Center the content vertically and horizontally */
 .home-page {
-  background-color: lightblue;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+/* Style for the main heading */
+h1 {
+  font-family: 'Monaco', sans-serif;
+  font-size: 35px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+/* Container for the centered boxes */
+.content {
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
-  min-height: 100vh; /* Adjust the subtraction value as needed */
-  margin: 0;
-  padding: 0;
+  flex-wrap: wrap; /* Allow boxes to wrap to the next row on smaller screens */
 }
 
-/* Rest of your styles for .centered-box, .logo, and text elements */
+/* Style for the centered boxes */
+.centered-box {
+  text-align: center;
+  padding: 20px;
+  border: 1px solid lightpink;
+  border-radius: 5px;
+  margin: 10px;
+  max-width: 300px;
+  background-color: #f5f5f5;
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1); /* Add a box shadow */
+}
 
-  
-  .centered-box {
-    text-align: center;
-    padding: 20px;
-    border: 1px solid lightpink;
-    border-radius: 5px;
-    margin: 10px;
-    max-width: 300px;
-    background-color: #f5f5f5;
-    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1); /* Add a box shadow */
-
-  }
-  .logo {
+/* Style for the logo */
+.logo {
   position: absolute;
-  bottom: 10px; /* Adjust the distance from the bottom as needed */
-  left: 10px; /* Adjust the distance from the left as needed */
+  bottom: 10px;
+  left: 10px;
 }
 
 .logo img {
-  width: 100px; /* Set the width of your logo */
-  height: auto; /* Maintain aspect ratio */
+  width: 100px;
+  height: auto;
 }
+
+/* Additional styles for text elements */
 h2 {
   font-family: 'Arial', sans-serif;
   font-size: 24px;
@@ -88,17 +109,5 @@ p {
   font-size: 16px;
   line-height: 1.5;
 }
-h1 {
-    font-family: 'Monaco', sans-serif;
-    font-size: 35px;
-    font-weight: bold;
-}
-/* Reset margins and padding for all elements */
-* {
-  margin: 0;
-  padding: 0;
-}
-
-
-  </style>
+</style>
   
