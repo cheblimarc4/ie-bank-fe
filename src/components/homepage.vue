cd2e739ce1f315db0814695a9c3eeb82f351e92b
cheblimarc4 <cheblimarc4@gmail.com>
2023-10-06 15:49:21 +0200

    hotfix 3

--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -3,36 +3,26 @@
       <div class="centered-box">
         <h2>Welcome to the Bank App</h2>
         <p>Would you like to go to the Accounts page to manage your finances?</p>
-        <button @click="goToAccounts">Go to Accounts</button>
+        <router-link to="/accounts">Go to Accounts</router-link>
       </div>
   
       <div class="centered-box">
         <h2>Explore the Skull Component</h2>
         <p>Check out our interactive Skull component for fun!</p>
-        <button @click="goToSkullComponent">Explore</button>
+        <router-link to="/skull-component">Explore</router-link>
       </div>
   
       <div class="centered-box creative-box">
         <h2>Be Creative!</h2>
         <p>Add your own creative content here.</p>
-        <button @click="exploreCreativeContent">Get Inspired</button>
+        <router-link to="/creative-content">Get Inspired</router-link>
       </div>
     </div>
   </template>
   
   <script>
   export default {
-    methods: {
-      goToAccounts() {
-        // Add your navigation logic to the Accounts page
-      },
-      goToSkullComponent() {
-        // Add your navigation logic to the Skull component
-      },
-      exploreCreativeContent() {
-        // Add your navigation logic for the creative content
-      },
-    },
+    name: 'Home',
   };
   </script>
   
